1cce74517a175c2ffbe55af51c8d478e43c56f03
CHAHYUNHO <chamym@naver.com>
2020-06-27 23:16:59 +0900

    first commit

---
+++ src/main/java/com/camel/camel_boot_ex/CamelBootEx1Application.java
@@ -0,0 +1,103 @@
+package com.camel.camel_boot_ex;
+
+import java.io.IOException;
+
+import org.apache.catalina.LifecycleException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootConfiguration
+// @Configuration
+// @ComponentScan
+// @EnableAutoConfiguration
+@SpringBootApplication
+public class CamelBootEx1Application {
+
+	public static void main(String[] args) throws IOException, LifecycleException {
+		SpringApplication application = new SpringApplication(CamelBootEx1Application.class);
+			application.setWebApplicationType(WebApplicationType.NONE);
+			application.run(args);
+			// SpringApplication.run(CamelBootEx1Application.class, args);
+		/*
+		  스프링 부트는 서버가 아니다.
+		  1. 톰캣 객체를 생성
+		  2. 포트 설정
+		  3. 톰캣 컨택스트 추가
+		  4. 서블릿 만들기
+		  5. 톰캣에 서블릿 추가
+		  6. 컨텍스트 서블릿 맵핑
+		  7. 톰캣 실행 및 대기 
+		  
+		  이 모든 과정 보다 상세히 또 유연하게 설정하고 실행해주는게 스프링 부트 자동 설정 
+			○ ServletWebServerFactoryAutoConfiguration (서블릿 웹 서버 생성) 
+			■ TomcatServletWebServerFactoryCustomizer (서버 커스터마이징) 
+			○ DispatcherServletAutoConfiguration 
+			■ 서블릿 만들고 등록 
+
+		  Tomcat tomcat = new Tomcat(); tomcat.setPort(8080);
+		  
+		  String docBase = Files.createTempDirectory("tomcat-basedir").toString();
+		  
+		  System.out.println(docBase);
+		  
+		  Context context = tomcat.addContext("/", docBase);
+		  
+		  HttpServlet servlet = new HttpServlet() {
+		  
+			@Override
+			protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+					throws ServletException, IOException {
+				PrintWriter writer = resp.getWriter();
+				writer.println("<head><title>");
+				writer.println("Hey, tomcat");
+				writer.println("<title></head>");
+				writer.println("<body><h1>Hello Tomcat</h1></body>");
+				}
+			};
+		  
+		  String servletName = "helloSevlet"; 
+		  tomcat.addServlet("/", servletName, servlet); 
+		  context.addServletMappingDecoded("/hello", servletName);
+		  
+		  tomcat.getConnector();
+		  tomcat.start(); 
+		  tomcat.getServer().await();
+		  
+		 */
+		 
+		
+	}
+	
+	
+	
+	
+	
+	
+	/*
+	@Bean
+	public Holoman holoman() {
+		Holoman holoman = new Holoman();
+		holoman.setName("CHAMYM");
+		holoman.setHowLong(10);
+		return holoman;
+	}
+	*/
+	
+	
+	/*
+	 * 현재 상황은 직접만든 메이븐 패키징을 이 프로젝트에 주입하여 
+	 * 해당 라이브러리에 빈을 설정하고 어플리케이션을 설정했다. 
+	 * 하지만 이프로젝트에 똑같은 빈설정(holoman)을 하게 되면 어떻게 될까에 대해 학습하는 내용이다.
+	 * 스프링 부트가 2.2로 올라가면서 빈 오버라이딩을 막아서 익셉션발생
+	 * 그래서 영상에서 설명하는 빈이 덮어 쓰여지는 현상이 벌어지질 않음 
+	 * `spring.main.allow-bean-definition-overriding=true` 
+	 * 설정을 추가해서 빈 오버라이딩을 허용하시면 수업 영상과 동일하게 
+	 * 실습을 진행할 수 있긴하지만 그냥 영상만 참고 하셔도 좋습니다.
+	 * 
+	 * 빈 오버라이딩을 허용하는방법은 
+	 * 패키징 했던 메이븐프로젝트 해당빈에  @ConditionalOnMissingBean 어노테이션을 사용하게되면
+	 * 현재 이프로젝트에 설정된 똑같은 빈으로 덮어 씌여지게 된다. 
+	 * */
+
+}
---
+++ src/test/java/com/camel/camel_boot_ex/CamelBootEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.camel_boot_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelBootEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
